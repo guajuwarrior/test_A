00e9ce989ddc6e84385bb3ce98abad00dae5bb6e
Cássio <Cssio@192.168.0.110>
2019-11-23 17:01:05 -0300

    test_A

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TESTE_GITHUB_A</title>
+    </head>
+    <body>
+        <h1>TESTE A</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
